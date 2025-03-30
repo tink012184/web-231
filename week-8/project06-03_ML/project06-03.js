492acb96559369707abd3e97a38139b088381c09
Tink <85289337+tink012184@users.noreply.github.com>
2025-03-30 00:55:03 -0500

    copy all remaining files for assignments

---
+++ week-8/project06-03_ML/project06-03.js
@@ -0,0 +1,12 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-03
+
+      Script to complete a form containing billing and shipping address information
+      Author: Melissa Lutz
+      Date:   
+
+      Filename: project06-03.js
+*/
+

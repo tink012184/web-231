31689b369c1b10dbb7ea7135bb9e7eb6e483851c
Tink <85289337+tink012184@users.noreply.github.com>
2025-05-08 19:04:48 -0500

    final upload for week 7

--- week-8/project06-03_ML/project06-03.js
+++ week-8/project06-03_ML/project06-03.js
@@ -5,8 +5,48 @@
 
       Script to complete a form containing billing and shipping address information
       Author: Melissa Lutz
-      Date:   
+      Date: 5/8/2025  
 
       Filename: project06-03.js
 */
 
+let useShip = document.getElementById("useShip");
+
+useShip.addEventListener("click", copyShippingToBilling);
+
+function copyShippingToBilling() {
+  if (document.getElementById("useShip").checked) {
+    document.getElementById("firstnameBill").value =
+      document.getElementById("firstnameShip").value;
+    document.getElementById("lastnameBill").value =
+      document.getElementById("lastnameShip").value;
+    document.getElementById("address1Bill").value =
+      document.getElementById("address1Ship").value;
+    document.getElementById("address2Bill").value =
+      document.getElementById("address2Ship").value;
+    document.getElementById("cityBill").value =
+      document.getElementById("cityShip").value;
+    document.getElementById("countryBill").value =
+      document.getElementById("countryShip").value;
+    document.getElementById("codeBill").value =
+      document.getElementById("codeShip").value;
+
+    document.getElementById("stateBill").selectedIndex =
+      document.getElementById("stateShip").selectedIndex;
+  } else {
+    return;
+  }
+}
+
+let formElements = document.querySelectorAll("input[type = 'text']");
+let fieldCount = formElements.length;
+let errorBox = document.getElementById("errorBox");
+
+for (let i = 0; i < fieldCount; i++) {
+  formElements[i].addEventListener("invalid", showValidationError);
+}
+
+function showValidationError(evt) {
+  evt.preventDefault();
+  errorBox.textContent = "Complete all highlighted fields";
+}

937213bf9ac1318dc7f236c25f6cffa4bd7da277
Tink <85289337+tink012184@users.noreply.github.com>
2025-05-15 14:18:15 -0500

    week 9 project

--- week-9/project07-01_ML/project07-01.js
+++ week-9/project07-01_ML/project07-01.js
@@ -5,16 +5,33 @@
 
       Project to validate a form used for setting up a new account
       Author: Melissa Lutz
-      Date:   
+      Date: 5/15/2025  
 
       Filename: project07-01.js
 */
 
 let signupForm = document.getElementById("signup");
 
-signupForm.addEventListener("submit", function(e) { 
-   let pwd = document.getElementById("pwd").value;
-   let feedback = document.getElementById("feedback");
+signupForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-}
-);
+  let pwd = document.getElementById("pwd").value;
+  let feedback = document.getElementById("feedback");
+
+  const regex1 = /[A-Z]/;
+  const regex2 = /\d/;
+  const regex3 = /[!$#%]/;
+
+  if (pwd.length < 8) {
+    feedback.textContent = "Your password must be at least 8 characters.";
+  } else if (!regex1.test(pwd)) {
+    feedback.textContent = "Your password must include an uppercase letter.";
+  } else if (!regex2.test(pwd)) {
+    feedback.textContent = "Your password must include a number.";
+  } else if (!regex3.test(pwd)) {
+    feedback.textContent =
+      "Your password must include one of the following: !$#%.";
+  } else {
+    signupForm.submit();
+  }
+});
